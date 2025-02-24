00dd8e28dfda9fe55b9a261e3bcc47de6d95219e
Rafael Puchades <rpuchadm@cfp.upv.es>
2025-02-24 23:52:08 +0100

    auth

--- main.go
+++ main.go
@@ -5,8 +5,10 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
 	"os"
+	"strings"
 	"time"
 
 	_ "github.com/lib/pq"
@@ -30,6 +32,12 @@ func initTables(connStr string) http.HandlerFunc {
 			return
 		}
 		err = initTableAuthClients(db)
+		if err != nil {
+			http.Error(w, fmt.Sprintf("{\"error\",\"%v\"}", err), http.StatusInternalServerError)
+			return
+		}
+
+		err = initTableAuthProfiles(db)
 		if err != nil {
 			http.Error(w, fmt.Sprintf("{\"error\",\"%v\"}", err), http.StatusInternalServerError)
 			return
@@ -66,7 +74,40 @@ func initTablePersons(db *sql.DB) error {
 	return nil
 }
 
-// inintTableAuthClients crea la tabla "auth_clients" si no existe
+// initTableAuthClients crea la tabla "auth_clients" si no existe
+
+// initTableAuthProfilea crea la tabla "auth_profiles" si no existe
+// esta tabla se usa para almacenar los perfiles de autenticación
+// a los que se accederá por token de manera reiterada
+// después de que se acceda una vez por code
+func initTableAuthProfiles(db *sql.DB) error {
+
+	// SQL para crear la tabla si no existe
+	// code y token son únicos
+	// json_data es un campo jsonb que puede almacenar cualquier información adicional
+	// created_at es la fecha de creación del registro
+	createTableSQL := `
+		CREATE TABLE IF NOT EXISTS auth_profiles (
+			id SERIAL PRIMARY KEY,
+			code VARCHAR(32) UNIQUE,
+			token VARCHAR(255) UNIQUE,
+			json_data JSONB,
+			created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+		);`
+
+	// Ejecuta la creación de la tabla
+	_, err := db.Exec(createTableSQL)
+	if err != nil {
+		return fmt.Errorf("error al crear la tabla auth_profiles: %v", err)
+	}
+
+	//CREATE UNIQUE INDEX unique_code ON auth_profiles (code) WHERE code IS NOT NULL;
+	//CREATE UNIQUE INDEX unique_token ON auth_profiles (token) WHERE token IS NOT NULL;
+	return nil
+}
+
+// initTableAuthClients crea la tabla "auth_clients" si no existe
+// esta tabla se usa para almacenar los clientes que pueden acceder a la API
 func initTableAuthClients(db *sql.DB) error {
 
 	// SQL para crear la tabla si no existe
@@ -110,6 +151,12 @@ func dropTables(connStr string) http.HandlerFunc {
 			return
 		}
 
+		err = dropTableAuthProfiles(db)
+		if err != nil {
+			http.Error(w, fmt.Sprintf("{\"error\",\"%v\"}", err), http.StatusInternalServerError)
+			return
+		}
+
 		// Responde json indicando que las tablas se eliminaron o no existían
 		w.Write([]byte(`{"message": "Tablas eliminadas o no existían"}`))
 	}
@@ -137,6 +184,19 @@ func dropTableAuthClients(db *sql.DB) error {
 	_, err := db.Exec(dropTableSQL)
 	if err != nil {
 		return fmt.Errorf("error al eliminar la tabla auth_clients: %v", err)
+	}
+	return nil
+}
+
+func dropTableAuthProfiles(db *sql.DB) error {
+
+	// SQL para eliminar la tabla "auth_profiles"
+	dropTableSQL := `DROP TABLE IF EXISTS auth_profiles;`
+
+	// Ejecuta la eliminación de la tabla
+	_, err := db.Exec(dropTableSQL)
+	if err != nil {
+		return fmt.Errorf("error al eliminar la tabla auth_profiles: %v", err)
 	}
 	return nil
 }
@@ -228,6 +288,13 @@ func main() {
 	http.HandleFunc("/auth", withLogging(corsMiddleware(withAuth(getAuthHandler, auth_token))))
 	http.HandleFunc("/persons", withLogging(corsMiddleware(withAuth(getPersonsHandler(connStr), auth_token))))
 	http.HandleFunc("/person", withLogging(corsMiddleware(withAuth(postPersonHandler(connStr), auth_token))))
+
+	// post json con client_id y client_url
+	http.HandleFunc("/authinit", withLogging(corsMiddleware(withAuth(postAuthInitHandler(connStr), auth_token))))
+	// token?code=123
+	http.HandleFunc("/token", withLogging(corsMiddleware(getAuthTokenHandler(connStr))))
+	http.HandleFunc("/profile", withLogging(corsMiddleware(getAuthProfileHandler(connStr))))
+
 	http.HandleFunc("/init", withLogging(initTables(connStr)))
 	http.HandleFunc("/clean", withLogging(dropTables(connStr)))
 	http.HandleFunc("/status", withLogging(checkTable(connStr)))
@@ -377,6 +444,216 @@ func postPersonHandler(connStr string) http.HandlerFunc {
 		w.Write([]byte(`{"message": "Persona creada", "id": ` + fmt.Sprintf("%d", id) + `}`))
 	}
 }
+
+func authHeader(r *http.Request) string {
+	authorization := r.Header.Get("Authorization")
+	if authorization != "" {
+		// quitamos el prefijo Bearer
+		return authorization[7:]
+	}
+	return ""
+}
+
+func postAuthInitHandler(connStr string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		// Abre la conexión a la base de datos
+		var err error
+		db, err := openDatabaseConnection(connStr)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer db.Close()
+
+		// Verifica que el método sea POST
+		if r.Method != http.MethodPost {
+			http.Error(w, `{"error": "Método no permitido"}`, http.StatusMethodNotAllowed)
+			return
+		}
+
+		// Parsea el cuerpo de la solicitud en json
+		var data map[string]interface{}
+		if err := json.NewDecoder(r.Body).Decode(&data); err != nil {
+			http.Error(w, fmt.Sprintf(`{"error": "Error al parsear el cuerpo de la solicitud: %v"}`, err), http.StatusBadRequest)
+			return
+		}
+
+		// Verifica que los campos no estén vacíos
+		if data["client_id"] == "" || data["client_url"] == "" {
+			http.Error(w, `{"error": "Los campos client_id y client_url son requeridos"}`, http.StatusBadRequest)
+			return
+		}
+		if data["user_id"] == "" || data["user_type"] == "" {
+			http.Error(w, `{"error": "Los campos user_id y user_type son requeridos"}`, http.StatusBadRequest)
+			return
+		}
+
+		// se genera un code aleatorio
+		code := generaCode()
+
+		// SQL para insertar un mensaje
+		var id int
+		query := `INSERT INTO auth_profiles (code, json_data) VALUES ($1, $2) RETURNING id;`
+		jsonData, err := json.Marshal(data)
+		if err != nil {
+			http.Error(w, fmt.Sprintf(`{"error": "Error al convertir el json: %v"}`, err), http.StatusInternalServerError)
+			return
+		}
+		err = db.QueryRow(query, code, jsonData).Scan(&id)
+		if err != nil {
+			http.Error(w, fmt.Sprintf(`{"error": "Error al insertar el perfil de autenticación: %v"}`, err), http.StatusInternalServerError)
+			return
+		}
+
+		// Responde con un mensaje en formato JSON
+		w.Write([]byte(`{"message": "Perfil de autenticación creado", "code": "` + code + `"}`))
+	}
+}
+
+func getAuthProfileHandler(connStr string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		authToken := authHeader(r)
+		if authToken == "" {
+			http.Error(w, `{"error": "No autorizado"}`, http.StatusUnauthorized)
+			return
+		}
+
+		// Abre la conexión a la base de datos
+		var err error
+		db, err := openDatabaseConnection(connStr)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer db.Close()
+
+		// Verifica que el método sea GET
+		if r.Method != http.MethodGet {
+			http.Error(w, `{"error": "Método no permitido"}`, http.StatusMethodNotAllowed)
+			return
+		}
+
+		// SQL para obtener json_data de auth_profiles por token
+		query := `SELECT json_data FROM auth_profiles where token = $1;`
+		rows, err := db.Query(query, authToken)
+		if err != nil {
+			http.Error(w, fmt.Sprintf(`{"error": "Error al obtener el perfil de autenticación: %v"}`, err), http.StatusInternalServerError)
+			return
+		}
+		defer rows.Close()
+
+		// Estructura para almacenar los perfiles de autenticación
+		var jsonData string
+		if rows.Next() {
+			if err := rows.Scan(&jsonData); err != nil {
+				http.Error(w, fmt.Sprintf(`{"error": "Error al escanear el perfil de autenticación: %v"}`, err), http.StatusInternalServerError)
+				return
+			}
+		}
+
+		// Responde con los perfiles de autenticación en formato JSON
+		w.Header().Set("Content-Type", "application/json")
+		w.Write([]byte(jsonData))
+	}
+}
+
+func getAuthTokenHandler(connStr string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		// a partir del parámetro code se obtiene el id de auth_profile de la URL
+		// se genera un token aleatorio y se almacena en auth_profiles
+		// y se pone a null el campo code
+
+		// Abre la conexión a la base de datos
+		var err error
+		db, err := openDatabaseConnection(connStr)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer db.Close()
+
+		// Verifica que el método sea GET
+		if r.Method != http.MethodGet {
+			http.Error(w, `{"error": "Método no permitido"}`, http.StatusMethodNotAllowed)
+			return
+		}
+
+		// se obtiene el parámetro code de la URL
+		code := r.URL.Query().Get("code")
+		if code == "" {
+			http.Error(w, `{"error": "El parámetro code es requerido"}`, http.StatusBadRequest)
+			return
+		}
+
+		// SQL para obtener el token de auth_profiles por code
+		query := `SELECT id FROM auth_profiles WHERE code = $1;`
+		rows, err := db.Query(query, code)
+		if err != nil {
+			http.Error(w, fmt.Sprintf(`{"error": "Error al obtener el token de autenticación: %v"}`, err), http.StatusInternalServerError)
+			return
+		}
+		defer rows.Close()
+
+		// Estructura para almacenar los perfiles de autenticación
+		var id int
+		if rows.Next() {
+			if err := rows.Scan(&id); err != nil {
+				http.Error(w, fmt.Sprintf(`{"error": "Error al escanear el token de autenticación: %v"}`, err), http.StatusInternalServerError)
+				return
+			}
+		}
+
+		// si no se encuentra el id se responde con error
+		if id == 0 {
+			http.Error(w, `{"error": "No se encontró el token de autenticación"}`, http.StatusNotFound)
+			return
+		}
+
+		// se genera un token aleatorio
+		token := generaToken()
+
+		// SQL para actualizar el token de auth_profiles por id
+		query = `UPDATE auth_profiles SET token = $1, code = NULL WHERE id = $2;`
+		_, err = db.Exec(query, token, id)
+		if err != nil {
+			http.Error(w, fmt.Sprintf(`{"error": "Error al actualizar el token de autenticación: %v"}`, err), http.StatusInternalServerError)
+			return
+		}
+
+		// Responde con el token de autenticación en formato JSON
+		w.Header().Set("Content-Type", "application/json")
+		w.Write([]byte(`{"token": "` + token + `"}`))
+	}
+}
+
+func generaCode() string {
+	return generaRand(32)
+}
+
+func generaToken() string {
+	return generaRand(255)
+}
+
+func generaRand(length int) string {
+	const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	sb := strings.Builder{}
+	sb.Grow(length)
+	for i := 0; i < length; i++ {
+		sb.WriteByte(letterBytes[r.Intn(len(letterBytes))])
+	}
+	return sb.String()
+}
+
+/*
+type AuthProfileData struct {
+	ID        int       `json:"id"`
+	Code      string    `json:"code"`
+	Token     string    `json:"token"`
+	JsonData  string    `json:"json_data"`
+	CreatedAt time.Time `json:"created_at"`
+}
+*/
 
 // Middleware para registrar solicitudes HTTP
 func withLogging(handler http.HandlerFunc) http.HandlerFunc {
